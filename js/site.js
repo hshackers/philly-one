0fa1644b6b5e12cc48fe62554bc0d6f741f773ed
Victor Lourng <hello@victorlourng.com>
2014-04-15 00:28:59 -0400

    Scripts

---
+++ js/site.js
@@ -0,0 +1,18 @@
+$( document ).ready(function() {
+  var s = skrollr.init();
+  console.log( "Bored? http://www.youtube.com/watch?v=dQw4w9WgXcQ!" );
+});
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
